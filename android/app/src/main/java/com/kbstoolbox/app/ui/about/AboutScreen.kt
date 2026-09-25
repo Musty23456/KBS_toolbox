6413f91a04a8e7b50833dcfd1225044bb24f586a
Musty23456 <Mustaphasalisu093@gmail.com>
2026-09-25 10:44:25 +0100

    Create AboutScreen with project credits
    Added an AboutScreen with credits and acknowledgments.

---
+++ android/app/src/main/java/com/kbstoolbox/app/ui/about/AboutScreen.kt
@@ -0,0 +1,28 @@
+        KBS Toolbox
+
+      [ Mustapha Photo ]
+
+       Mustapha Salisu
+    Project Creator
+
+
+   Special Appreciation
+
+      [ Suraj Photo ]
+
+       Abubakar Suraj
+   SIWES Coordinator
+      My Inspirator
+
+
+       Special Thanks
+
+        KBS Staff
+
+
+        Development
+
+Created by Mustapha Salisu
+in collaboration with colleagues
+and with the assistance of
+modern Artificial Intelligence.
